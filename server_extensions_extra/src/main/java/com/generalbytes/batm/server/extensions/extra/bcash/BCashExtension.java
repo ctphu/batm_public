264b37f4c5d03858dccd608c2a6227009bf48dcd
ctphu <phu.t.cao@outlook.com>
2018-12-25 14:15:00 +0700

    Add bcash folder

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bcash/BCashExtension.java
@@ -0,0 +1,120 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2016 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.bcash;
+
+import com.generalbytes.batm.server.extensions.*;
+import com.generalbytes.batm.server.extensions.FixPriceRateSource;
+import com.generalbytes.batm.server.extensions.extra.bcash.sources.cdbcash.CryptodiggersRateSource;
+import com.generalbytes.batm.server.extensions.extra.bcash.sources.coinmarketcap.CoinmarketcapRateSource;
+import com.generalbytes.batm.server.extensions.extra.bcash.wallets.bcashd.BCashRPCWallet;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class BCashExtension extends AbstractExtension{
+    @Override
+    public String getName() {
+        return "BATM BCash extra extension";
+    }
+
+    @Override
+    public IWallet createWallet(String walletLogin) {
+        if (walletLogin !=null && !walletLogin.trim().isEmpty()) {
+            StringTokenizer st = new StringTokenizer(walletLogin,":");
+            String walletType = st.nextToken();
+
+            if ("bcashd".equalsIgnoreCase(walletType)) {
+                //"bcashd:protocol:user:password:ip:port:accountname"
+
+                String protocol = st.nextToken();
+                String username = st.nextToken();
+                String password = st.nextToken();
+                String hostname = st.nextToken();
+                String port = st.nextToken();
+                String accountName ="";
+                if (st.hasMoreTokens()) {
+                    accountName = st.nextToken();
+                }
+
+
+                if (protocol != null && username != null && password != null && hostname !=null && port != null && accountName != null) {
+                    String rpcURL = protocol +"://" + username +":" + password + "@" + hostname +":" + port;
+                    return new BCashRPCWallet(rpcURL,accountName);
+                }
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public ICryptoAddressValidator createAddressValidator(String cryptoCurrency) {
+        if (Currencies.BCASH.equalsIgnoreCase(cryptoCurrency)) {
+            return new BcashAddressValidator();
+        }
+        return null;
+    }
+
+    @Override
+    public IRateSource createRateSource(String sourceLogin) {
+        if (sourceLogin != null && !sourceLogin.trim().isEmpty()) {
+            StringTokenizer st = new StringTokenizer(sourceLogin, ":");
+            String exchangeType = st.nextToken();
+            if ("cdbcash".equalsIgnoreCase(exchangeType)) {
+                if (st.hasMoreTokens()) {
+                    return new CryptodiggersRateSource(st.nextToken().toUpperCase());
+                }
+                return new CryptodiggersRateSource(Currencies.USD);
+            } else if ("bcashfix".equalsIgnoreCase(exchangeType)) {
+                BigDecimal rate = BigDecimal.ZERO;
+                if (st.hasMoreTokens()) {
+                    try {
+                        rate = new BigDecimal(st.nextToken());
+                    } catch (Throwable e) {
+                    }
+                }
+                String preferedFiatCurrency = Currencies.USD;
+                if (st.hasMoreTokens()) {
+                    preferedFiatCurrency = st.nextToken().toUpperCase();
+                }
+                return new FixPriceRateSource(rate, preferedFiatCurrency);
+            } else if ("coinmarketcap".equalsIgnoreCase(exchangeType)) {
+                String preferedFiatCurrency = Currencies.USD;
+                if (st.hasMoreTokens()) {
+                    preferedFiatCurrency = st.nextToken().toUpperCase();
+                }
+                return new CoinmarketcapRateSource(preferedFiatCurrency);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Set<String> getSupportedCryptoCurrencies() {
+        Set<String> result = new HashSet<String>();
+        result.add(Currencies.BTC);
+        result.add(Currencies.BTX);
+        result.add(Currencies.BCH);
+        result.add(Currencies.LTC);
+        result.add(Currencies.XMR);
+        result.add(Currencies.DASH);
+		result.add(Currencies.BCASH);
+        result.add(Currencies.POT);
+        return result;
+    }
+}
